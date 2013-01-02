fc2f660b7369f0f0ba5c7386d88ec419b00cf9e5
Alvaro del Castillo <acs@barrapunto.com>
2013-01-02 16:35:32 +0100

    Changed license to GPLv3

--- src/Report.js
+++ src/Report.js
@@ -1,20 +1,24 @@
 /* 
- * 
- * Copyright (C) 2012, Bitergia
+ * Copyright (C) 2012 Bitergia
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * Report.js: Library for visualizing Bitergia Reports
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This file is a part of the VizGrimoireJS package
+ *
+ * Authors:
+ *   Alvaro del Castillo San Felix <acs@bitergia.com>
  */
 
 var Report = {};
