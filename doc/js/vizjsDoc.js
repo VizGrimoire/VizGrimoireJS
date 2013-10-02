8a76dbfb9e13089012842b63b362c5de72049d19
Alvaro del Castillo <acs@bitergia.com>
2013-10-02 18:28:31 +0200

    Prototype for doc working. Time to implement all.

--- doc/js/vizjsDoc.js
+++ doc/js/vizjsDoc.js
@@ -1,7 +1,27 @@
 var vizjsDoc = {};
 
 (function() {
-    $.getJSON("vizjsapi2.json", null, function(history) {
-        vizjsDoc.API = history;
-    });
+    // Build a new div with method info and convert it
+    vizjsDoc.showDiv = function(divDisplay, method) {
+        var new_div = "<div id='"+method+"'>"+method+"</div>";
+        $("#"+divDisplay).empty();
+        $("#"+divDisplay).append(new_div);
+        Report.convertGlobal();
+        Report.convertStudies();
+    };
+    
+    vizjsDoc.showSections = function(divid, divdisplay) {
+        $.getJSON("vizjsapi2.json", null, function(apidata) {
+            var sections = "";
+            $.each(apidata.sections, function(section, contents) {
+                sections += "<h4>"+section+"<h4>";
+                $.each(contents, function(method, method_desc) {
+                    sections += "<button onClick='";
+                    sections += "vizjsDoc.showDiv(\""+divdisplay+"\",\""+method+"\")";
+                    sections += "' class='btn'>"+method+"</button>";
+                });
+            });
+            $("#"+divid).append(sections);
+        });
+    };
 })();
